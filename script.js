d362a402f031299adc77b8226c2d16f16988d1d5
JD <jorgedaniel.fcamelo@gmail.com>
2025-08-08 21:47:14 -0300

    Staring with events

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-let nomeUsuario = "JD";
-const cpf = "18734567780";
+const lista = document.querySelector("[data-list]");
+
+const novaTarefa = document.querySelector("[data-form-button]");
+
+// Criar Tarefa
+const criarTarefa = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+    const valor = input.value;
+    console.log(valor);
+}
+
+
+// Evento
+novaTarefa.addEventListener('click', criarTarefa);
+
+
+
+
+
+
+
+
+
+
