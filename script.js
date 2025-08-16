cb26faf75497078f95bd822c3c4e25368deee9a7
JD <jorgedaniel.fcamelo@gmail.com>
2025-08-15 21:46:43 -0300

    adicionei anotações e desenvolvi o javascript

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-const lista = document.querySelector("[data-list]");
-
-const novaTarefa = document.querySelector("[data-form-button]");
-
-// Criar Tarefa
 const criarTarefa = (evento) => {
     evento.preventDefault();
+    
+    const lista = document.querySelector("[data-list]");
     const input = document.querySelector("[data-form-input]");
     const valor = input.value;
-    console.log(valor);
+
+    const tarefa = document.createElement(`li`);
+    tarefa.classList.add('task');
+    const conteudo = `<p class="content">${valor}</p>`;
+
+    tarefa.innerHTML = conteudo;
+    tarefa .appendChild(BotaoConclui());
+    lista.appendChild(tarefa);
+
+
+    input.value = "";
 }
 
 
+
 // Evento
+const novaTarefa = document.querySelector("[data-form-button]");
 novaTarefa.addEventListener('click', criarTarefa);
 
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement('button');
 
+    botaoConclui.classList.add('check-button');
+    botaoConclui.innerText = 'concluir';
+
+    botaoConclui.addEventListener('click', ()=> {
+        console.log('fui clicado');
+
+    });
+    return botaoConclui;
+
+}
 
 
 
